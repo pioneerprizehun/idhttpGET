b61ecd388e8e91b4e4e42546356f89ea6e96b618
SangPro <bjc1382@gmail.com>
2022-09-23 11:46:13 +0900

    no message

---
+++ Unit1.dfm
@@ -0,0 +1,59 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 405
+  ClientWidth = 738
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 8
+    Top = 24
+    Width = 193
+    Height = 57
+    Caption = 'idHTTP GET'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object StringGrid1: TStringGrid
+    Left = 8
+    Top = 87
+    Width = 713
+    Height = 310
+    ColCount = 6
+    TabOrder = 1
+  end
+  object IdHTTP1: TIdHTTP
+    ProxyParams.BasicAuthentication = False
+    ProxyParams.ProxyPort = 0
+    Request.ContentLength = -1
+    Request.ContentRangeEnd = -1
+    Request.ContentRangeStart = -1
+    Request.ContentRangeInstanceLength = -1
+    Request.Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+    Request.BasicAuthentication = False
+    Request.UserAgent = 'Mozilla/3.0 (compatible; Indy Library)'
+    Request.Ranges.Units = 'bytes'
+    Request.Ranges = <>
+    HTTPOptions = [hoForceEncodeParams]
+    Left = 680
+    Top = 16
+  end
+  object IdSSLIOHandlerSocketOpenSSL1: TIdSSLIOHandlerSocketOpenSSL
+    MaxLineAction = maException
+    Port = 0
+    DefaultPort = 0
+    SSLOptions.Mode = sslmUnassigned
+    SSLOptions.VerifyMode = []
+    SSLOptions.VerifyDepth = 0
+    Left = 640
+    Top = 16
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,128 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, IdBaseComponent, IdComponent,
+  IdTCPConnection, IdTCPClient, IdHTTP, Vcl.StdCtrls, System.JSON, Data.DB,
+  Vcl.Grids, Vcl.DBGrids, IdAuthentication, IdIOHandler, IdIOHandlerSocket,
+  IdIOHandlerStack, IdSSL, IdSSLOpenSSL;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    IdHTTP1: TIdHTTP;
+    IdSSLIOHandlerSocketOpenSSL1: TIdSSLIOHandlerSocketOpenSSL;
+    StringGrid1: TStringGrid;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+const Url = 'https://api.odcloud.kr/api/15092258/v1/uddi:b209f7aa-2f4f-4843-aaa9-93ffe4187449?page=1&perPage=10&serviceKey=7KIl6dMmzuL4vfVttT1tRb1mfi%2BOvK6F8gtf0bOSYBQAeHBJBJsWhuJtUX7FkIFv5u1XOFDjlOwe%2Bc78mk76XA%3D%3D';
+
+//====================================스트링그리드 컬럼크기자동조절================================
+procedure AutoSizeGridColumn(Grid : TStringGrid; column : integer);
+var
+  i : integer;
+  temp : integer;
+  max : integer;
+begin
+  max := 0;
+  for i := 0 to (Grid.RowCount-1) do
+  begin
+    // Grid Canvas를 기준으로한 지정한 Column의 각 row의 width중
+    // 최대값을 구하여 column의 width로 결정한다
+    temp := Grid.Canvas.TextWidth(grid.cells[column, i]);
+    if temp > max then
+      max := temp;
+  end;
+  Grid.ColWidths[column] := max + Grid.GridLineWidth + 20;
+end;
+//====================================스트링그리드 컬럼크기자동조절 끝=============================
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+stream: TStringStream;
+idHttpObj: TIdHTTP;
+JSONValue : TJSONValue;
+JSONArray : TJSONArray;
+JSONObject, JSONdata: TJSONObject;
+JSONCount: integer;
+i: integer;
+r: integer;
+begin
+stream := TStringStream.Create('', TEncoding.UTF8);
+
+idHttpObj := TIdHTTP.Create(nil);
+idHttpObj.Get(Url, stream);
+idHttpObj.Free;
+
+JSONValue := TJSONObject.ParseJSONValue(stream.DataString);
+
+
+//*************************************************************
+
+{배열카운터}
+JSONObject := TJSONObject.ParseJSONValue(stream.DataString) as TJSONObject;
+JSONArray := JSONObject.GetValue('data') as TJSONArray;
+JSONCount := JSONArray.Count;
+{배열카운터끝}
+
+//***********************************************************
+StringGrid1.RowCount := JSONCount+1;
+{StringGrid1에 윗부분셀추가}
+StringGrid1.Cells[00,0] := '의소대별';
+StringGrid1.Cells[01,0] := '정원(남성대)';
+StringGrid1.Cells[02,0] := '정원(여성대)';
+StringGrid1.Cells[03,0] := '정원(총)';
+StringGrid1.Cells[04,0] := '현원(남성대)';
+StringGrid1.Cells[05,0] := '현원(여성대)';
+StringGrid1.Cells[06,0] := '현원(총)';
+{StringGrid1에 윗부분셀추가 끝}
+
+//*************************************************************
+
+{StringGrid1 셀크기 자동조절}
+for i := 0 to StringGrid1.ColCount-1 do
+begin
+AutoSizeGridColumn(StringGrid1, i);
+end;
+{StringGrid1 셀크기 자동조절 끝}
+
+//*************************************************************
+
+{for문을 이용한 Memo1, StringGrid1 내용추가}
+for i := 0 to Integer(JSONCount)-1 do
+begin
+  StringGrid1.Cells[0,i+1] := (JSONValue.GetValue<string>('data['+i.ToString+'].의소대별'));
+  StringGrid1.Cells[1,i+1] := (JSONValue.GetValue<string>('data['+i.ToString+'].정원(남성대)'));
+  StringGrid1.Cells[2,i+1] := (JSONValue.GetValue<string>('data['+i.ToString+'].정원(여성대)'));
+  StringGrid1.Cells[3,i+1] := (JSONValue.GetValue<string>('data['+i.ToString+'].정원(총)'));
+  StringGrid1.Cells[4,i+1] := (JSONValue.GetValue<string>('data['+i.ToString+'].현원(남성대)'));
+  StringGrid1.Cells[5,i+1] := (JSONValue.GetValue<string>('data['+i.ToString+'].현원(여성대)'));
+  StringGrid1.Cells[6,i+1] := (JSONValue.GetValue<string>('data['+i.ToString+'].현원(총)'));
+
+
+
+end;
+
+{for문을 이용한 Memo1, StringGrid1 내용추가 끝}
+
+//*************************************************************
+{Memo1 내용추가 끝}
+  stream.Free;     //stream free선언
+
+end;
+
+end.
